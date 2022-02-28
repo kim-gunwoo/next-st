bc6ecb17115093ddeb0c3d48a9aca6d93056578c
김건우 <kgunu93@gmail.com>
2022-02-28 10:42:05 +0900

    add room register building

---
+++ c-bnb/pages/room/register/bathroom.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import RegisterBathroom from "../../../components/room/register/RegisterRoomBathroom";
+
+const bathroom: NextPage = () => {
+  return <RegisterBathroom />;
+};
+
+export default bathroom;
