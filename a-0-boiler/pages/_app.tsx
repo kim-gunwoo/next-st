18c099f9f7725f2f87d5bb5c35110b35ff8a7ba4
김건우 <kgunu93@gmail.com>
2022-04-07 15:48:50 +0900

    add redux

--- a-0-boiler/pages/_app.tsx
+++ a-0-boiler/pages/_app.tsx
@@ -1,4 +1,6 @@
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
+import App from "next/app";
+import { wrapper } from "store";
 import { globalStyles } from "styles";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -10,4 +12,26 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+MyApp.getInitialProps = wrapper.getInitialAppProps(
+  (store) => async (context: AppContext) => {
+    const appInitialProps = await App.getInitialProps(context);
+    // const cookieObj = cookies(context.ctx);
+    // const cookieObject = cookieStringToObject(context.ctx.req?.headers.cookie);
+    // const { isLogged } = store.getState().user;
+    // console.log("wrapper.getInitialAppProps : ", cookieObject, cookieObj);
+
+    try {
+      // if (!isLogged && cookieObj.access_token) {
+      //   axios.defaults.headers.common["Cookie"] = cookieObj.access_token;
+      //   const { data } = await meAPI();
+      //   store.dispatch(userActions.setLoggedUser(data));
+      //   return { pageProps: {} };
+      // }
+    } catch (e: any) {
+      // console.log(e.message);
+    }
+    return { ...appInitialProps };
+  }
+);
+
+export default wrapper.withRedux(MyApp);
---
+++ a-0-boiler/store/index.ts
@@ -0,0 +1,39 @@
+import { HYDRATE, createWrapper } from "next-redux-wrapper";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  TypedUseSelectorHook,
+  useSelector as useReduxSelector,
+} from "react-redux";
+
+const rootReducer = combineReducers({});
+
+//* 스토어의 타입
+export type RootState = ReturnType<typeof rootReducer>;
+
+let initialRootState: RootState;
+
+const reducer = (state: any, action: any) => {
+  if (action.type === HYDRATE) {
+    if (state === initialRootState) {
+      return {
+        ...state,
+        ...action.payload,
+      };
+    }
+    return state;
+  }
+  return rootReducer(state, action);
+};
+
+//* 타입 지원되는 커스텀 useSelector 만들기
+export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
+
+const initStore = (context: any) =>
+  configureStore({
+    reducer,
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware()],
+    devTools: process.env.NODE_ENV !== "production",
+  });
+
+export const wrapper = createWrapper(initStore);
