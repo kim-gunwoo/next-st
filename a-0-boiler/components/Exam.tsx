afee27dfd4762ff7db421b23fb47b585b8556c48
김건우 <kgunu93@gmail.com>
2022-04-14 09:26:04 +0900

    add storybook

--- a-0-boiler/components/Exam.tsx
+++ a-0-boiler/components/Exam.tsx
@@ -1,5 +1,7 @@
+import { css } from "@emotion/react";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
+import ButtonExamEmotion from "./ButtonExamEmotion";
 
 const Exam: React.FC = () => {
   const { isExample } = useSelector((state: RootState) => state.example);
@@ -8,8 +10,14 @@ const Exam: React.FC = () => {
     <div className="cnt">
       <div className="title">isExample redux Example Page</div>
       {isExample ? <div>true</div> : <div>false</div>}
+      <ButtonExamEmotion css={buttonStyle} />
+      <ButtonExamEmotion />
     </div>
   );
 };
+
+const buttonStyle = css`
+  background: blue;
+`;
 
 export default Exam;
