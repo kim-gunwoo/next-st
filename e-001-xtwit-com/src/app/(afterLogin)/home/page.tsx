e2248726dcb361fea6c36ca87d479050fee26c96
git-k <kgunu93@gmail.com>
2024-02-20 21:38:52 +0900

    feat: home main

--- e-001-xtwit-com/src/app/(afterLogin)/home/page.tsx
+++ e-001-xtwit-com/src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,15 @@
+import PostForm from "./_component/PostForm";
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+import style from "./home.module.css";
+
 export default function Page() {
-  return <div>home page</div>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+      </TabProvider>
+    </main>
+  );
 }
