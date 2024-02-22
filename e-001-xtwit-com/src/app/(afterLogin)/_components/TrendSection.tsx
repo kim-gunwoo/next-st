7467c884849db1397ef80e7478ecec2f1fab7a44
git-k <kgunu93@gmail.com>
2024-02-22 21:50:15 +0900

    feat: right section zone

--- e-001-xtwit-com/src/app/(afterLogin)/_components/TrendSection.tsx
+++ e-001-xtwit-com/src/app/(afterLogin)/_components/TrendSection.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Trend from "./Trend";
 import style from "./trendSection.module.css";
 
 export default function TrendSection() {
+  const pathname = usePathname();
+  if (pathname === "/explore") return null;
+
   return (
     <div className={style.trendBg}>
       <div className={style.trend}>
