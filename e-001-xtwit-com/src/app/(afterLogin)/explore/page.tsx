b0e3ffeb39df27469255d4f6776fa36051b1db25
git-k <kgunu93@gmail.com>
2024-02-22 21:53:09 +0900

    feat: add explore

--- e-001-xtwit-com/src/app/(afterLogin)/explore/page.tsx
+++ e-001-xtwit-com/src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,26 @@
+import style from "./explore.module.css";
+import SearchForm from "../_components/SearchForm";
+import Trend from "../_components/Trend";
+
 export default function Page() {
-  return <div>explore page</div>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
