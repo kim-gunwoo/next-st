0482208b267a00be6ebf4d84c42f6ac0a8057c5e
git-k <kgunu93@gmail.com>
2024-02-23 19:40:17 +0900

    feat: add profile page

--- e-001-xtwit-com/src/app/(afterLogin)/[username]/page.tsx
+++ e-001-xtwit-com/src/app/(afterLogin)/[username]/page.tsx
@@ -1,3 +1,38 @@
+import style from "./profile.module.css";
+import BackButton from "../_components/BackButton";
+import Post from "../_components/Post";
+
 export default function Page() {
-  return <div>username page</div>;
+  const user = {
+    id: "test",
+    nickname: "test",
+    image: "/example2.jpg",
+  };
+
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={user.image} alt={user.id} />
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>@{user.id}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
 }
--- e-001-xtwit-com/src/app/(afterLogin)/search/page.tsx
+++ e-001-xtwit-com/src/app/(afterLogin)/search/page.tsx
@@ -1,3 +1,37 @@
-export default function Page() {
-  return <div>search page</div>;
+import style from "./search.module.css";
+import BackButton from "../_components/BackButton";
+import Post from "../_components/Post";
+import SearchForm from "../_components/SearchForm";
+import Tab from "./_component/Tab";
+
+type Props = {
+  searchParams: { q: string; f?: string; pf?: string };
+};
+
+export default function Page({ searchParams }: Props) {
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
 }
