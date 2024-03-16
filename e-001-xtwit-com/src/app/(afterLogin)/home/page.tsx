3c741888ebe26252a21bfbf4e9a250c65c9713e4
git-k <kgunu93@gmail.com>
2024-03-16 11:13:21 +0900

    feat: add pkg query

--- e-001-xtwit-com/src/app/(afterLogin)/home/page.tsx
+++ e-001-xtwit-com/src/app/(afterLogin)/home/page.tsx
@@ -1,22 +1,34 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import Post from "../_components/Post";
 import PostForm from "./_component/PostForm";
 import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
 import style from "./home.module.css";
+import PostRecommends from "./_component/PostRecommends";
+import { getPostRecommends } from "./_lib/getPostRecommends";
 
-export default function Page() {
+export default async function Page() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <main className={style.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </TabProvider>
-    </main>
+    <HydrationBoundary state={dehydratedState}>
+      <main className={style.main}>
+        <TabProvider>
+          <Tab />
+          <PostForm />
+          <PostRecommends />
+        </TabProvider>
+      </main>
+    </HydrationBoundary>
   );
 }
--- e-001-xtwit-com/src/mocks/handlers.ts
+++ e-001-xtwit-com/src/mocks/handlers.ts
@@ -1,4 +1,20 @@
 import { http, HttpResponse } from "msw";
+import { faker } from "@faker-js/faker";
+
+function generateDate() {
+  const lastWeek = new Date(Date.now());
+  lastWeek.setDate(lastWeek.getDate() - 7);
+  return faker.date.between({
+    from: lastWeek,
+    to: Date.now(),
+  });
+}
+
+const User = [
+  { id: "test-1", nickname: "test one name", image: "/example.jpg" },
+  { id: "test-2", nickname: "test two name", image: "/example2.jpg" },
+  { id: "test-3", nickname: "test three name", image: faker.image.avatar() },
+];
 
 export const handlers = [
   http.post("/api/login", () => {
@@ -35,5 +51,57 @@ export const handlers = [
     //     "Set-Cookie": "connect.sid=msw-cookie;HttpOnly;Path=/;Max-Age=0",
     //   },
     // });
+  }),
+  http.get("/api/postRecommends", async ({ request }) => {
+    const cursor = 0;
+    return HttpResponse.json([
+      {
+        postId: cursor + 1,
+        User: User[0],
+        content: `${cursor + 1} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: cursor + 2,
+        User: User[0],
+        content: `${cursor + 2} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [
+          { imageId: 1, link: faker.image.urlLoremFlickr() },
+          { imageId: 2, link: faker.image.urlLoremFlickr() },
+        ],
+        createdAt: generateDate(),
+      },
+      {
+        postId: cursor + 3,
+        User: User[0],
+        content: `${cursor + 3} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [],
+        createdAt: generateDate(),
+      },
+      {
+        postId: cursor + 4,
+        User: User[0],
+        content: `${cursor + 4} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [
+          { imageId: 1, link: faker.image.urlLoremFlickr() },
+          { imageId: 2, link: faker.image.urlLoremFlickr() },
+          { imageId: 3, link: faker.image.urlLoremFlickr() },
+          { imageId: 4, link: faker.image.urlLoremFlickr() },
+        ],
+        createdAt: generateDate(),
+      },
+      {
+        postId: cursor + 5,
+        User: User[0],
+        content: `${cursor + 5} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [
+          { imageId: 1, link: faker.image.urlLoremFlickr() },
+          { imageId: 2, link: faker.image.urlLoremFlickr() },
+          { imageId: 3, link: faker.image.urlLoremFlickr() },
+        ],
+        createdAt: generateDate(),
+      },
+    ]);
   }),
 ];
